8e2735ed99e418b3c37fd981d1572158fbc2e2aa
Viking <Viking0607@mail.ru>
2015-06-05 13:35:29 +0500

    DB commit

---
+++ DB_request/DB_mobile/очистка.sql
@@ -0,0 +1,24 @@
+truncate table continue_links
+/
+truncate table bad_news_links
+/
+truncate table logs
+/
+truncate table news
+/
+Drop sequence NEWS_SEQ
+/
+Create sequence NEWS_SEQ
+/
+/
+Drop sequence bad_news_links_seq
+/
+Create sequence bad_news_links_seq
+/
+Drop sequence continue_news_links_seq
+/
+Create sequence continue_news_links_seq
+/
+drop sequence logs_seq
+/
+create sequence logs_seq
