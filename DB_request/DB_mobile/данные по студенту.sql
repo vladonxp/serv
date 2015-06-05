720fefaf38c8c9468e90e9c3f8c3716df902fd7b
Viking <Viking0607@mail.ru>
2015-06-05 15:47:05 +0500

    commit DB req

--- DB_request/DB_mobile/данные по студенту.sql
+++ DB_request/DB_mobile/данные по студенту.sql
@@ -6,9 +6,9 @@ select * from mv_stud_appoint st
         upper(replace(replace('якимчук јлександр ¬.','.',''),' ','')),1)>=1
 
 ------------------------------------------------------------------------------------------
-Create view V_STUD_APPOINT as
+--Create view V_STUD_APPOINT as
 Select FPERSONS, FFIO,FTABNMB,FSEX,FCOURSE,ZACH,FAK,SPEC,GRUP,BUD   from mv_stud_appoint
-Where FFIO like 'якимчук%'
+Where FFIO='якимчук јлександр ¬асильевич'
 ---------------------------------------------------------------------------------------------
 
 
