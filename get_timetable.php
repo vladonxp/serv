461b890ca31c5696fe2574283cccdb78b430eb2a
vladonxp <vladonxp@mail.ru>
2015-06-05 16:04:59 +0500

    add getting timetable

---
+++ get_timetable.php
@@ -0,0 +1,16 @@
+﻿<?php 
+  Header("Content-Type: text/html;charset=UTF-8");
+  require_once('oracle/database_connect.php');
+  require_once('auth/ad_functions.php');
+
+  $sql = "select TEAC_FIO, TEAC_CAF, DISCIPLINE, SUBGRUP, DATEZAN, DAYWEEK, PAIR, VID, AUD, KORP, START_PAIR, FINISH_PAIR from V_TIMETABLE 
+                  where GR_NUM = '1521б'";
+  
+  $s = OCIParse($c,$sql);
+  OCIExecute($s, OCI_DEFAULT);
+  ocifetchstatement($s, $data_news_arr);
+  
+  print_r(str_replace('\/','/',json_encode_cyr($data_news_arr)));
+  
+
+?>
